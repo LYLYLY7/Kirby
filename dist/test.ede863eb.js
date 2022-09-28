9ef611a28bc6c2af0c03c20adf883c8ae3801e10
LYLYLY <lylyly72020@163.com>
2022-09-28 10:27:27 +0800

    实现播放暂停速度功能

--- dist/test.ede863eb.js
+++ dist/test.ede863eb.js
@@ -1,3 +1,4 @@
+const string = `
 * {
   box-sizing: border-box;
   margin: 0;
@@ -256,3 +257,50 @@
   background-color: #dd4545;
   z-index: 1;
 }
+`;
+let n = 1;
+// substring 子字符串（起始，末尾）
+demo.innerHTML = string.substring(0, n);
+demo2.innerText = string.substring(0, n);
+console.log(n);
+let time = 0;
+const run = ()=>{
+    n += 1;
+    if (n > string.length) {
+        window.clearInterval(id);
+        return;
+    }
+    console.log(n + ":" + string.substring(0, n));
+    demo.innerText = string.substring(0, n);
+    demo2.innerHTML = string.substring(0, n);
+    demo.scrollTop = demo.scrollHeight;
+};
+let id = setInterval(()=>{
+    run();
+}, time);
+//上述代码可以改写为
+// let id =setInterval(run,time)
+// ()=>{fn()} 等价于 fn()
+btnPause.onclick = ()=>{
+    window.clearInterval(id);
+};
+btnPlay.onclick = ()=>{
+    id = setInterval(run, time);
+};
+btnSlow.onclick = ()=>{
+    window.clearInterval(id);
+    time = 300;
+    id = setInterval(run, time);
+};
+btnNormal.onclick = ()=>{
+    window.clearInterval(id);
+    time = 100;
+    id = setInterval(run, time);
+};
+btnFast.onclick = ()=>{
+    window.clearInterval(id);
+    time = 0;
+    id = setInterval(run, time);
+};
+
+//# sourceMappingURL=test.ede863eb.js.map
